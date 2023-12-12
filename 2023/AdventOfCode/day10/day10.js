904450a5bf70532f795714eadc47c740f0667049
orozcoding <orozcoding@gmail.com>
2023-12-12 09:26:18 -0400

    Finish day 10 part 1, almost complete part 2, to revisit later

--- 2023/AdventOfCode/day10/day10.js
+++ 2023/AdventOfCode/day10/day10.js
@@ -24,13 +24,14 @@ const findSPosition = (map) => {
 
 // if you reach this type of pipe, and you come from this side of the pide
 // where can it take you to?
+// for PART 2, list this clockwise direction order (i.e. for '-', show west to east movement first)
 const TYPE_TO_DEST_FOR_DIRECTION = {
   '|': {'north': 'south', 'south': 'north'},
   '-': {'west': 'east', 'east': 'west'},
-  'L': {'north': 'east', 'east': 'north'},
+  'L': {'east': 'north', 'north': 'east'},
   'J': {'north': 'west', 'west': 'north'},
   '7': {'west': 'south', 'south': 'west'},
-  'F': {'east': 'south', 'south': 'east'}
+  'F': {'south': 'east', 'east': 'south'}
 }
 
 const DEST_TO_NEW_COORDS = {
@@ -79,10 +80,66 @@ const countForRoundMove = (map, startingCoords, startingType) => {
 // console.log(countForRoundMove(map, findSPosition(map), SType))
 
 
-const SType = '-' // manually checked
-const countToFarthest = countForRoundMove(map, findSPosition(map), SType) / 2
-console.log(countToFarthest) // correct 7012 for PART 1 ⭐️
+const SType = '7' // manually checked, code fails if input.txt changes
+// const countToFarthest = countForRoundMove(map, findSPosition(map), SType) / 2
+// console.log(countToFarthest) // correct 7012 for PART 1 ⭐️
 
 
+// PART 2
+// "If you go clockwise, everything to the right side is inside"
+
+const RIGHT_SIDE_OF_DIRECTION = {
+  'north': 'east',
+  'east': 'south',
+  'south': 'west',
+  'west': 'north'
+}
 
 
+const rightTile = (coords, directionUsed) => {
+  const sideToCheck = RIGHT_SIDE_OF_DIRECTION[directionUsed]
+  return move(coords, sideToCheck)
+}
+
+// console.log(isNextToTile(map, [7,3], 'west'))
+
+const uniqueArrayFrom = (arr) => {
+  const uniq = []
+  const obj = {}
+  arr.forEach((el) => {
+    if(obj[el]) return
+    obj[el] = el
+    uniq.push(el)
+  })
+  return uniq
+}
+
+
+const countTitlesInside = (map, startingCoords, startingType) => {
+    // start at any possible direction, and move clockwise
+    const coordsInLoop = [startingCoords]
+    let nextDirection = Object.values(TYPE_TO_DEST_FOR_DIRECTION[startingType])[0]
+    const tilesToTheRightOfLoop = [rightTile(startingCoords, nextDirection)]
+    let coords = move(startingCoords, nextDirection)
+    coordsInLoop.push(coords)
+    tilesToTheRightOfLoop.push(rightTile(coords,nextDirection))
+    let currentPipe = map[coords[0]][coords[1]]
+    while(currentPipe != 'S') {
+      nextDirection = nextMoveDirection(SWITCH_DIRECTIONS[nextDirection], currentPipe)
+      coords = move(coords, nextDirection)
+      currentPipe = map[coords[0]][coords[1]]
+      coordsInLoop.push(coords)
+      tilesToTheRightOfLoop.push(rightTile(coords, nextDirection))
+    }
+
+    const tiles = tilesToTheRightOfLoop.filter((el) => !coordsInLoop.some((cel) => cel[0] == el[0] && cel[1] == el[1]))
+    return tiles.length
+}
+
+console.log(countTitlesInside(map,findSPosition(map), SType))
+
+// turns out that anything could be count as inside the loop. Doesn't need to be a '.'
+
+
+// TODO, FOR EVERY F GOING TO SOUTH, COORDS AT NORTH ARE TITLES (BESIDES THE WEST COORDS IT WILL CHECK)
+// FOR EVERY 7 GOING TO WEST, COORDS AT EAST ARE TILES (BESIDES THE NORTH COORDS IT WILL CHECK)
